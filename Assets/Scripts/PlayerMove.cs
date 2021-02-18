147da2daf3c4c7340876ce7c96b50570f2923c37
dabandabanda <kristaps.loc@gmail.com>
2021-02-18 15:36:54 +0000

    First Commit
    Adding All Files

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float jumpSpeed = 20;
+    Rigidbody2D RB;
+    Animator animatior;
+    float playerScale;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+   
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        animatior = GetComponent<Animator>();
+        playerScale = transform.localScale.x;
+    }      
+
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            RB.AddForce(Vector2.right * moveSpeed);
+            animatior.SetBool("isWalking", true);
+            transform.localScale = new Vector3(playerScale, transform.localScale.y, 0);
+           
+        }
+         if (Input.GetKey("a"))
+        {
+            RB.AddForce(Vector2.left * moveSpeed);
+            animatior.SetBool("isWalking", true);
+            transform.localScale = new Vector3(-playerScale, transform.localScale.y, 0);
+        }         
+
+        else if (RB.velocity.magnitude <= 0.5f)
+        {
+            animatior.SetBool("isWalking", false);
+        }
+
+        Jump();
+    }
+    void Jump()
+    {
+        Collider2D groundCollider = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+      
+        if (groundCollider != null)
+        {
+            if (Input.GetButtonDown ("Jump"))
+            {
+                RB.AddForce(Vector2.up * jumpSpeed,ForceMode2D.Impulse);
+            }
+        }           
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Level4_2D_Unity_Game_2021
-Level4 2D Unity Game 2021 Spring
+# ZoomUnitySession1
+ Level 5 Unity Session
