427819b32134945f3397c18afeaa5ef6c06cf47a
dabandabanda <kristaps.loc@gmail.com>
2021-02-19 10:40:02 +0000

    ScoreManager Script added

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+
+    ScoreManager scoreManager;
+
+    private void Start()
+    {
+        scoreManager = FindObjectOfType<ScoreManager>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Debug.Log("Object Collected!");
+            scoreManager.ObjectCollected();
+           
             Destroy(gameObject, 0.5f);
         }
     }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int Score;
+
+    public void ObjectCollected()
+    {
+        Score++;
+
+        Debug.Log("Current Score: " + Score);
+
+    }   
+
+
+}
