120367f91c955b93c6a0e2d71e47d5644ee4ffc5
dabandabanda <kristaps.loc@gmail.com>
2021-03-05 13:46:51 +0000

    Session 6 - Part 4

---
+++ Assets/OutOfBounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    HealthManager healthManager;
+    Transform player;
+    Transform fallCheck;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthManager = FindObjectOfType<HealthManager>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        fallCheck = GameObject.FindGameObjectWithTag("FallCheck").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (player.position.y <= fallCheck.position.y)
+        {
+            healthManager.Damage(100);
+        }
+    }
+}
